05506579d3501da10d66be224e5081d21d4d96d5
Felipe Cesar Sepulvida Rampazzo <ff.cc.ss.rr@gmail.com>
2025-01-26 20:14:08 -0300

    Alterando body response

--- video-frame-pro-upload-lambda.py
+++ video-frame-pro-upload-lambda.py
@@ -1,38 +1,12 @@
 import boto3
-import uuid
 import urllib.request
 from urllib.error import URLError, HTTPError
 import json
-from datetime import datetime
 
 s3_client = boto3.client("s3")
-dynamodb = boto3.resource("dynamodb")
-sqs = boto3.client("sqs")
 
 BUCKET_NAME = "video-frame-pro"
-SQS_QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/935588215946/video-frame-pro-processing"
-DYNAMO_PROCESSES_TABLE = "video-frame-pro-processes"
-DYNAMO_METADATA_TABLE = "video-frame-pro-metadata"
-COGNITO_PUBLIC_KEY = "x"
 VIDEO_CONTENT_TYPE= "video/mp4"
-
-def validate_cognito_token(token):
-    client = boto3.client('cognito-idp')
-    
-    try:
-        response = client.get_user(AccessToken=token)
-        
-        email = next(attr["Value"] for attr in response["UserAttributes"] if attr["Name"] == "email")
-        username = response["Username"]
-        
-        return {
-            "email": email,
-            "username": username
-        }
-    except client.exceptions.NotAuthorizedException:
-        raise ValueError("Token inválido ou expirado.")
-    except Exception as e:
-        raise ValueError(f"Erro ao validar o token no Cognito: {str(e)}")
 
 def validate_video_link(video_url):
     try:
@@ -71,24 +45,16 @@ def download_video(video_url):
 
 def lambda_handler(event, context):
     try:
-        video_url = event["video_url"]
-        frame_rate = int(event["frame_rate"])
-        # token = event["token"]
-
-        # user_info = validate_cognito_token(token)
-        # user_email = user_info["email"]
-        # username = user_info["username"]
-
+        process_id = event["videoId"]
+        username = event["username"]
+        video_url = event["videoLink"]
         user_email = 'ff.cc.ss.rr@gmail.com'
-        username = 'Felipe Rampazzo'
 
         validate_video_link(video_url)
 
         video_content = download_video(video_url)
 
-        process_id = str(uuid.uuid4())
-
-        video_key = f"videos/{process_id}/{process_id}-source.mp4"
+        video_key = f"videos/{username}/{process_id}/{process_id}-source.mp4"
         s3_client.put_object(
             Bucket = BUCKET_NAME, 
             Key = video_key, 
@@ -96,49 +62,13 @@ def lambda_handler(event, context):
             ContentType = VIDEO_CONTENT_TYPE
         )
 
-        processes_table = dynamodb.Table(DYNAMO_PROCESSES_TABLE)
-        current_time = datetime.utcnow().isoformat()
-
-        processes_table.put_item(
-            Item={
-                "ID_PROCESS": process_id,
-                "DAT_CREATION": current_time,
-                "DAT_FINISHED": None,
-                "DAT_UPDATE": current_time,
-                "DES_STATUS": "PROCESSING",
-            }
-        )
-
-        metadata_table = dynamodb.Table(DYNAMO_METADATA_TABLE)
-        metadata_id = str(uuid.uuid4())
-
-        metadata_table.put_item(
-            Item={
-                "ID_METADATA": metadata_id,
-                "ID_PROCESS": process_id,
-                "DES_USER_EMAIL": user_email,
-                "NAM_USERNAME": username,
-                "NAM_VIDEO": video_key,
-                "NUM_FRAME_RATE": frame_rate,
-            }
-        )
-
-        sqs.send_message(
-            QueueUrl=SQS_QUEUE_URL,
-            MessageBody=json.dumps({
-                "object_key": video_key,
-                "user_name": username,
-                "to_address": user_email,
-                "frame_rate": frame_rate,
-            })
-        )
-
         return {
             "statusCode": 200,
             "body": json.dumps({
-                "message": "Processamento iniciado com sucesso.",
-                "process_id": process_id,
-                "video_key": video_key,
+                "videoId": process_id,
+                "username": username,
+                "videoLink": video_url,
+                "email": user_email
             })
         }
 
